7925f5f68813ee042fda456d9a8ecb789849f32b
unknown <ramjancse@gmail.com>
2022-07-07 20:15:49 +0600

    functionality added

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const showGalleryElem = document.getElementById('showGallery');
+const nextBtnElem = document.getElementById('next');
+const prevBtnElem = document.getElementById('prev');
+const searchElem = document.getElementById('search');
+let page = 1; 
+const limit = 20;
 
-
-    fetch('http://localhost:3000/galary/')
+searchElem.addEventListener('change', (event)=> {
+    const searchParam = event.target.value;
+    fetch(`http://localhost:3000/galary?_sort=${searchParam}`, {
+    header: {
+        'Access-Control-Allow-Origin':'http://localhost:3000',
+    }
+})
     .then((response) => response.json())
     .then((data) => {
         showGalleryElem.innerHTML = '';
         data.forEach((item) => {
             let showData  = `
-            <div class="col s12 m6 l4 xl3">
+            <div class="col s12 m6 l6 xl6">
             <div class="card">
                 <div class="card-image" id="${item.id}" onClick=myFunction(${item.id})>
                     <img src="${item.image}" />
                 </div>
-                <div class="card-content">
+                <div class="card-content truncate">
                     <h5> ${item.title} </h5>
-                <p>
+                <p class="truncate">
                    ${item.description}
                 </p>
                 </div>
@@ -30,11 +40,68 @@ const showGalleryElem = document.getElementById('showGallery');
     })
     .catch((error) => console.log(error));
 
+})
+
+
+function fetchData() {
+    fetch(`http://localhost:3000/galary?_page=${page}&limit=${limit}`, {
+    header: {
+        'Access-Control-Allow-Origin':'http://localhost:3000',
+    }
+})
+    .then((response) => response.json())
+    .then((data) => {
+        showGalleryElem.innerHTML = '';
+        data.forEach((item) => {
+            let showData  = `
+            <div class="col s12 m6 l6 xl6">
+            <div class="card">
+                <div class="card-image" id="${item.id}" onClick=myFunction(${item.id})>
+                    <img src="${item.image}" />
+                </div>
+                <div class="card-content truncate">
+                    <h5> ${item.title} </h5>
+                <p class="truncate">
+                   ${item.description}
+                </p>
+                </div>
+                <div class="card-action">
+                    Rating :${item.rating}
+                </div>
+            </div>
+            </div>
+            `
+            showGalleryElem.insertAdjacentHTML('afterbegin',
+                showData);
+        });        
+    })
+    .catch((error) => console.log(error));
+}
+
+fetchData();
+
 function myFunction(id) {
     const indvID = document.getElementById(id);
     const findClass = indvID.querySelector('.imageScale');
     indvID.classList.toggle('imageScale');
 }
-function next() {
+
+nextBtnElem.addEventListener('click', () => {
+    if (page>1) {
+        prevBtnElem.classList.remove('disabled');
+    }
+    page += 1;
+    console.log(page)
+    fetchData();
     
-}
+})
+prevBtnElem.addEventListener('click', () => {
+    if (page == 1) {
+        prevBtnElem.classList.add('disabled');    
+    } else {
+        page -= 1;
+        console.log(page)
+        fetchData();
+    }
+    
+})
